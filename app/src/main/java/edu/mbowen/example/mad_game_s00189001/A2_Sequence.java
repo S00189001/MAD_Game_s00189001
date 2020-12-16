6be9fd7491867e10df304ac7660ec7408c66b30f
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2020-12-16 00:45:10 +0000

    Adding Activities
    - Added Main Menu
    - Added Sequence View

---
+++ app/src/main/java/edu/mbowen/example/mad_game_s00189001/A2_Sequence.java
@@ -0,0 +1,14 @@
+package edu.mbowen.example.mad_game_s00189001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class A2_Sequence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a2__sequence);
+    }
+}
--- app/src/main/java/edu/mbowen/example/mad_game_s00189001/MainActivity.java
+++ app/src/main/java/edu/mbowen/example/mad_game_s00189001/MainActivity.java
@@ -3,6 +3,7 @@ package edu.mbowen.example.mad_game_s00189001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+    public void StartGame(View _view)
+    {
+
+    }
+
+    public void HighScores(View _view)
+    {
+
+    }
+
+    public void QuitGame(View _view)
+    {
+        System.exit(0);
+    }
+
 }
